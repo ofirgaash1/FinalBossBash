ccc3b7ca0c123a78c43ce9fada610e017b76a672
ofirgaash1 <ofir.gaash@gmail.com>
2025-01-19 10:03:05 +0200

    Update cleanup.sh
    check if runs as crontab

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,15 @@
-#!/usr/bin/bash
+#!/bin/bash
 
+# Check if running as root
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Running as root."
+
+    # Check if the script is running interactively
+    if [[ -t 0 ]]; then
+        echo "Running interactively."
+    else
+        echo "Running from crontab or another non-interactive source."
+    fi
+else
+    echo "Not running as root, script may not have all required privileges."
+fi
