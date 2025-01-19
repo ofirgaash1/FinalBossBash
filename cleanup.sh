9597bc626a4daadd51c2b8593c8f2e36a4da7ac0
ofirgaash1 <ofir.gaash@gmail.com>
2025-01-19 10:19:26 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -8,8 +8,25 @@ if [ "$(id -u)" -eq 0 ]; then
     if [[ -t 0 ]]; then
         echo "Running interactively."
     else
-        echo "Running from crontab or another non-interactive source."
+        rootAndNotInteractive=true;
     fi
 else
     echo "Not running as root, script may not have all required privileges."
+fi
+
+# rootAndNotInteractive essentially means it's running as crontab operation (?)
+if [ rootAndNotInteractive = "true" ]; then
+
+    shopt -s globstar
+
+    # directories mentioned by vlad, seperated by spaces
+    directories="/tmp /var/tm /var/log"
+
+    # Current date in seconds since the epoch
+    current_seconds=$(date +%s)
+
+    # Days threshold for file modification, as mentioned by vlad
+    days_threshold=17
+
+    seconds_threshold=$((days_threshold * 86400))
 fi
