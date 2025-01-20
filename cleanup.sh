c7d479cbd7c57505cba7d8f12404234f7a0cae7f
ofirgaash1 <ofir.gaash@gmail.com>
2025-01-20 03:35:33 +0200

    Update cleanup.sh
    logic to remove 'old' files

--- cleanup.sh
+++ cleanup.sh
@@ -8,15 +8,15 @@ if [ "$(id -u)" -eq 0 ]; then
     if [[ -t 0 ]]; then
         echo "Running interactively."
     else
-        rootAndNotInteractive=true;
+        rootAndNotInteractive=true
     fi
 else
     echo "Not running as root, script may not have all required privileges."
 fi
 
+rootAndNotInteractive=true
 # rootAndNotInteractive essentially means it's running as crontab operation (?)
-if [ rootAndNotInteractive = "true" ]; then
-
+if [ $rootAndNotInteractive = "true" ]; then
     shopt -s globstar
 
     # directories mentioned by vlad, seperated by spaces
@@ -29,4 +29,26 @@ if [ rootAndNotInteractive = "true" ]; then
     days_threshold=17
 
     seconds_threshold=$((days_threshold * 86400))
+
+    for directory in $directories; do
+        for file in "$directory"/**; do
+
+            # Continue only if it is a file
+            if [ -f "$file" ]; then
+
+                # last mod in sec since the epoch
+                last_modification_seconds=$(stat --format=%Y "$file")
+
+                # last mod since now in days
+                last_modification_in_days=$(((current_seconds - last_modification_seconds) / 86400))
+
+                # Check if the file is older than the threshold
+                if [ $last_modification_in_days -ge $days_threshold ]; then
+                    echo "Deleting $file modified $last_modification_in_days days ago."
+                    rm "$file"
+                fi
+                
+            fi
+        done
+    done
 fi
