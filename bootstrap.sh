d5740e9940682bf5e567736700a1ce481fbf60a7
ofirgaash1 <ofir.gaash@gmail.com>
2025-01-22 01:59:15 +0200

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+prefix="/usr/local/bin"
+
+files=('./menu.sh' './backup.sh' './cleanup.sh' './monitor.sh')
+for file in $files[@]; do
+    cp $file $prefix
+done
+
+
+# create cron jobs
+(
+    # print existing crontab
+    crontab -l 2>/dev/null
+
+    echo "0 * * * * $prefix/monitor.sh"
+    echo "0 1 4,20 * * $prefix/backup.sh"
+    echo "0 1 1 * * $prefix/cleanup.sh"
+) | crontab -
