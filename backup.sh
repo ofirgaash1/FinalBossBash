ce89968816e3f1bf0fc37f2d6160767bf4f6e935
ofirgaash1 <ofir.gaash@gmail.com>
2025-01-21 15:02:22 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,26 +1,61 @@
 #!/usr/bin/bash
 
-TARGET='/home'
+manualBackupORshowLastFiveLogs=$1
 
-# Read total size in bytes of a directory into array
-read -r -a dir_size_in_bytes_a <<<"$(du -sb "$TARGET")"
+# Check if running as root
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Running as root."
 
-dir_size_in_bytes="${dir_size_in_bytes_a[0]}"
-
-echo "$TARGET directory is overall $dir_size_in_bytes bytes"
-
-available_free_space_in_bytes=$(df --output=avail / | tail -1)
-
-echo "You have total $available_free_space_in_bytes free bytes on /"
-
-if [ $dir_size_in_bytes -lt $available_free_space_in_bytes ]; then
-    "backup.sh: no enough space for a backup. $(date)" >>/var/log/backup.log
+    # Check if the script is running interactively
+    if [[ -t 0 ]]; then
+        echo "Running interactively."
+        if
+        interactive=true
+    else
+        notInteractive=true
+    fi
+else
+    echo "please run this script as root".
     exit 1
 fi
 
-format="+%Y_%m_%d_%H_%M_%S_home_backup.tar.gzip"
-date_formated_string="$(date "$format")"
+if [ $manualBackupORshowLastFiveLogs = "manualBackup" || notInteractive = "true"]; then
+        
+    TARGET='/home'
 
-tar czf "/opt/sysmonitor/backups/$date_formated_string" --ignore-failed-read $TARGET 2>/dev/null 2>&1
+    # Read total size in bytes of a directory into array
+    read -r -a dir_size_in_bytes_a <<<"$(du -sb "$TARGET")"
 
-"backup.sh: home backed-up successfully. $(date)" >>/var/log/backup.log
+    dir_size_in_bytes="${dir_size_in_bytes_a[0]}"
+
+    echo "$TARGET directory is overall $dir_size_in_bytes bytes"
+
+    available_free_space_in_bytes=$(df --output=avail / | tail -1)
+
+    echo "You have total $available_free_space_in_bytes free bytes on /"
+
+    if [ $dir_size_in_bytes -lt $available_free_space_in_bytes ]; then
+        "backup.sh: no enough space for a backup. $(date)" >>/var/log/backup.log
+        exit 1
+    fi
+
+    format="+%Y_%m_%d_%H_%M_%S_home_backup.tar.gzip"
+    date_formated_string="$(date "$format")"
+
+    tar czf "/opt/sysmonitor/backups/$date_formated_string" --ignore-failed-read $TARGET 2>/dev/null 2>&1
+
+    "backup.sh: home backed-up successfully. $(date)" >>/var/log/backup.log
+
+elif [ "$manualBackupORshowLastFiveLogs" = "showLastFiveLogs" ]; then
+
+    file_name='/var/log/backup.log'
+
+    # -s option returns true when file exists and not empty
+    if [[ ! -s "$file_name" ]]; then
+        echo "$file_name doesn't exist or is empty"
+
+    else
+        # tail command gets lines from the end of the file
+        tail -5 "$file_name"
+    fi
+fi
