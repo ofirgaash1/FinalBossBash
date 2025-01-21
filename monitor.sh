00084e472203e0b7115b3ab78d8b98b559a31132
ofirgaash1 <ofir.gaash@gmail.com>
2025-01-21 15:39:07 +0200

    Create monitor.sh
    check if interactive

---
+++ monitor.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# Check if running as root
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Running as root."
+
+    # Check if the script is running interactively
+    if [[ -t 0 ]]; then
+        echo "Running interactively."
+        interactive=true
+    else
+        notInteractive=true
+    fi
+else
+    echo "please run this script as root".
+    exit 1
+fi
+
+if [$notInteractive = "true"]; then
