a776195e941580fb2447f64d424a601dea876cd3
ofirgaash1 <ofir.gaash@gmail.com>
2025-01-21 14:23:10 +0200

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+TARGET='/home'
+
+# Read total size in bytes of a directory into array
+read -r -a dir_size_in_bytes_a <<<"$(du -sb "$TARGET")"
+
+dir_size_in_bytes="${dir_size_in_bytes_a[0]}"
+
+echo "$TARGET directory is overall $dir_size_in_bytes bytes"
+
+available_free_space_in_bytes=$(df --output=avail / | tail -1)
+
+echo "You have total $available_free_space_in_bytes free bytes on /"
+
+if [ $dir_size_in_bytes -lt $available_free_space_in_bytes ]; then
+    "backup.sh: no enough space for a backup. $(date)" >>/var/log/backup.log
+    exit 1
+fi
+
+format="+%Y_%m_%d_%H_%M_%S_home_backup.tar.gzip"
+date_formated_string="$(date "$format")"
+
+tar czf "/opt/sysmonitor/backups/$date_formated_string" --ignore-failed-read $TARGET 2>/dev/null 2>&1
