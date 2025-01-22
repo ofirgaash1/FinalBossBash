5b43f549f37c545b83e5ea81f2af7b5960d51f72
ofirgaash1 <ofir.gaash@gmail.com>
2025-01-22 04:58:15 +0200

    Update bootstrap.sh
    interactivity will from now on be passed as an argument from menu

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,7 @@ done
     # print existing crontab
     crontab -l 2>/dev/null
 
-    echo "0 * * * * $prefix/monitor.sh"
-    echo "0 1 4,20 * * $prefix/backup.sh"
-    echo "0 1 1 * * $prefix/cleanup.sh"
+    echo "0 * * * * $prefix/monitor.sh scheduled"
+    echo "0 1 4,20 * * $prefix/backup.sh scheduled"
+    echo "0 1 1 * * $prefix/cleanup.sh scheduled"
 ) | crontab -
